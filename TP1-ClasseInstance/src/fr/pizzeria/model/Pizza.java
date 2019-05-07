d398f6e9e9dac41c6b263bb33ed7e30e79f5985e
Battistini <battistini.anthony@gmail.com>
2019-05-07 21:32:48 +0200

    TP1

---
+++ TP1-ClasseInstance/src/fr/pizzeria/model/Pizza.java
@@ -0,0 +1,27 @@
+package fr.pizzeria.model;
+
+public class Pizza {
+	
+	private static int nextId = 0;
+	
+	public int id;
+	public String code;
+	public String libelle;
+	public double prix;
+	
+	public Pizza(String code, String libelle, double prix) {
+		this.id = Pizza.nextId++;
+		this.code = code;
+		this.libelle = libelle;
+		this.prix = prix;
+	}
+	public Pizza(int id, String code, String libelle, double prix) {
+		if(id >= Pizza.nextId)
+			nextId = id + 1;
+		this.id = id;
+		this.code = code;
+		this.libelle = libelle;
+		this.prix = prix;
+	}
+	
+}
