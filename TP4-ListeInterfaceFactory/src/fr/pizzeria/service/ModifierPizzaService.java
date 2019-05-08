c4f9d0cfc3b38101f1674da0a4aa62a31848a1c2
Battistini <battistini.anthony@gmail.com>
2019-05-08 18:24:11 +0200

    TP4

---
+++ TP4-ListeInterfaceFactory/src/fr/pizzeria/service/ModifierPizzaService.java
@@ -0,0 +1,32 @@
+package fr.pizzeria.service;
+
+import java.util.Scanner;
+
+import fr.pizzeria.dao.IPizzaDao;
+import fr.pizzeria.model.Pizza;
+
+public class ModifierPizzaService extends MenuService {
+
+	@Override
+	public void executeUC(Scanner scan, IPizzaDao dao) {
+		
+		System.out.println("Veuillez choisir le code de la pizza à modifier");
+		String code = scan.next();
+		if(!dao.pizzaExists(code))
+			System.out.println("Code inexistant");
+		else {
+			System.out.println("Veuillez saisir le nouveau code :");
+			String newCode = scan.next();
+			System.out.println("Veuillez saisir le nouveau nom :");
+			String newLibelle = scan.next();
+			System.out.println("Veuillez saisir le nouveau prix :");
+			double newPrix = scan.nextDouble();
+			Pizza pizzaModif = dao.findPizzaByCode(code);
+			pizzaModif.modif(newCode, newLibelle, newPrix);
+			dao.updatePizza(code, pizzaModif);
+			System.out.println("Modification effectuée");
+		}
+
+	}
+
+}
